0e84b16b5bfd8675b8e3f9c1c39554f3e3acf11e
joel <joelfiddes@gmail.com>
2019-05-21 09:31:16 +0200

    code flow edits

--- tmapp_run_evo.py
+++ tmapp_run_evo.py
@@ -385,7 +385,7 @@ def main(wd, simdir, model="GEOTOP"):
 #===============================================================================
 #	Make ensemble
 #===============================================================================
-	if config["ensemble"]["run"] == TRUE:
+	if config["ensemble"]["run"] == "TRUE":
 
 		fname1 = home + "/SUCCESS_PERTURB"
 		if os.path.isfile(fname1) == False: #NOT ROBUST
