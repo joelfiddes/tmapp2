9c4d0ef0b337b2d170b7368f6c20514b5165662c
joelfiddes <joelfiddes@gmail.com>
2019-10-03 17:13:20 +0200

    points dem/topo recode

--- rsrc/makeListpoints2_points.R
+++ rsrc/makeListpoints2_points.R
@@ -15,41 +15,33 @@ require(raster)
 args = commandArgs(trailingOnly=TRUE)
 wd=args[1] 
 shp.in=args[2]
-
+simdir=args[3]
 #====================================================================
 # PARAMETERS FIXED
 #====================================================================
 
 setwd(paste0(wd,'/predictors'))
-
-nfiles=list.files(paste0(wd, "/predictors/"), pattern='ele')
-lpvec=c()
-for (file in 1:length(nfiles)){
+shpIndex = as.numeric(unlist(strsplit(simdir,'g'))[2])
 
 
-predictors=list.files( pattern=paste0('*',file,'.tif$'))
+predictors=list.files( pattern=paste0('*.tif$'))
 print(predictors)
 rstack=stack(predictors)
 shp <- shapefile(shp.in)
-lp = extract(rstack,shp[file,])
-lon = shp@coords[file,1]
-lat = shp@coords[file,2]
+lp = extract(rstack,shp[shpIndex,])
+lon = shp@coords[shpIndex,1]
+lat = shp@coords[shpIndex,2]
 
-if (length(shp$Name[file]) >0){name=shp$Name[file]}
-id= 1:length(shp@coords[,1])
-name=id[file]
-id = id[file]
-if (length(shp$Name[file]) >0){name=shp$Name[file]}
-if (length(shp$IMIS_ST[file]) >0){name=shp$IMIS_ST[file]}
+if (length(shp$Name[shpIndex]) >0){name=shp$Name[shpIndex]}
+#id= 1:length(shp@coords[,1])
+name=shpIndex
+id = shpIndex
+if (length(shp$Name[shpIndex]) >0){name=shp$Name[shpIndex]}
+if (length(shp$IMIS_ST[shpIndex]) >0){name=shp$IMIS_ST[shpIndex]}
 surfRough=0.002
 tz=0
 lp = data.frame(id,lp, lon,lat, surfRough,tz, name)
 lp = na.omit(lp)
-names(lp)<-c('id','asp','ele','slp','svf', 'lon','lat', 'surfRough','tz', 'name')
-lpvec=rbind(lpvec,lp)
-}
-
-lpvec$svf <- round(lpvec$svf,2)
 
 write.csv(lpvec, '../listpoints.txt', row.names=FALSE)
 
