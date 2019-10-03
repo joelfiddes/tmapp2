3b5ccb6331c99d70e365613eda486562d9c85827
joelfiddes <joelfiddes@gmail.com>
2019-10-03 15:10:21 +0200

    points dem/topo recode

--- rsrc/prepClust_pointsSparse.R
+++ rsrc/prepClust_pointsSparse.R
@@ -10,8 +10,7 @@ nfiles=list.files(paste0(wd, "/predictors/"), pattern='ele')
 for (file in 1:length(nfiles)){
 
 	ele=raster(paste0(wd,"/predictors/ele",file,".tif"))
-	ic=i #formatC(i, width=5, flag='0')
-	simdir=paste0('g',ic)
+	simdir=paste0('g',file)
 	dir.create(paste0(wd,"/sim/",simdir), showWarnings=FALSE)
 	dir.create(paste0(wd,"/sim/",simdir,"/predictors"), showWarnings=FALSE)
 	setwd(paste0(wd,'/sim/', simdir,"/predictors"))
