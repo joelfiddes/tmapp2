6becaefcd24927f7d0354b13cf58b67bea8808d7
Joel Fiddes <joelfiddes@gmail.com>
2020-10-29 13:55:00 +0100

    hpc array code

--- slurm_tscale.sh
+++ slurm_tscale.sh
@@ -12,8 +12,8 @@
 #SBATCH -n 1 # one cores
 #SBATCH -t 02:00:00 # Running time of 2 days
 #SBATCH --mem 4000 # Memory request of 4 GB
-#SBATCH -o output/myArray_%A_%a.out # Standard output - write the console output to the output folder %A= Job ID, %a = task or Step ID
-#SBATCH -e error/myArray_%A_%a.err # Standard error -write errors to the errors folder and
+#SBATCH -o log_tscale.out # Standard output - write the console output to the output folder %A= Job ID, %a = task or Step ID
+#SBATCH -e log_tscale.err # Standard error -write errors to the errors folder and
 #SBATCH --array=1-100 # create a array from 1to16 and limit the concurrent runing task  to 50
 #SBATCH --mail-user=joelfiddes@gmail.com
 #SBATCH --mail-type=ALL  # Send me some mails when jobs end or fail.
