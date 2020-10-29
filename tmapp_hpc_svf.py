fa89202ee6e71efba32d39589cbd748254413fa5
Joel Fiddes <joelfiddes@gmail.com>
2020-10-29 13:12:06 +0100

    hpc array code

--- tmapp_hpc_svf.py
+++ tmapp_hpc_svf.py
@@ -11,6 +11,8 @@ config = ConfigObj(wd + "/config.ini")
 svfSectors = config['toposcale']['svfSectors']
 svfMaxDist = config['toposcale']['svfMaxDist']
 tscale_root = config['main']['tscale_root']  # path to tscaleV2 directory
+nclust = config['toposub']['nclust']
+tmapp_root=config['main']['srcdir'] 
 sys.path.insert(1, tscale_root)
 import tscale_lib as tlib
 
@@ -20,4 +22,6 @@ print("Computing SVF grid " + str(jobid))
 
 homes = sorted(glob.glob(wd+"/sim/*"))
 tlib.compute_svf(homes[grid-1], svfSectors, svfMaxDist)
+tlib.compute_surface(homes[grid-1] )
+tlib.toposub(tmapp_root, homes[grid-1], nclust)
 print("Grid " + str(jobid) + " done!")
