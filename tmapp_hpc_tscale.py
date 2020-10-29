117ab808639e4557a240d14a57f2117ef2a38bf0
Joel Fiddes <joelfiddes@gmail.com>
2020-10-29 13:58:02 +0100

    hpc array code

--- tmapp_hpc_tscale.py
+++ tmapp_hpc_tscale.py
@@ -1,14 +1,17 @@
-
 import sys
 import glob
 import os
 jobid = os.getenv('SLURM_ARRAY_TASK_ID')
 
+from configobj import ConfigObj
+config = ConfigObj(wd + "/config.ini")
+tscale_root = config['main']['tscale_root']  # path to tscaleV2 directory
+sys.path.insert(1, tscale_root)
+import tscale3D
 
 wd= sys.argv[1] #'/home/joel/sim/qmap/ch_tmapp_10/' 
 starti= sys.argv[2] 
 endi= sys.argv[3] 
-
 
 mylist = glob.glob(wd+'/forcing/SURF_*')
 mymonths =sorted([i.split('SURF_', 1)[1] for i in mylist])
