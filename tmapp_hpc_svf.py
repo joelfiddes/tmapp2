4a3706e3cc7edac1dde2e243aca5ca8caa9481c8
Joel Fiddes <joelfiddes@gmail.com>
2020-10-29 11:04:07 +0100

    hpc array code

--- tmapp_hpc_svf.py
+++ tmapp_hpc_svf.py
@@ -2,15 +2,19 @@ import sys
 import os
 import glob
 from configobj import ConfigObj
+config = ConfigObj(wd + "/config.ini")
+svfSectors = config['toposcale']['svfSectors']
+svfMaxDist = config['toposcale']['svfMaxDist']
+tscale_root = config['main']['tscale_root']  # path to tscaleV2 directory
+sys.path.insert(1, tscale_root)
+import tscale_lib as tlib
 
 wd= sys.argv[1] #'/home/joel/sim/qmap/ch_tmapp_10/' 
 grid= sys.argv[2] 
 jobid = os.getenv('SLURM_ARRAY_TASK_ID')
 
 print("Computing SVF grid " + str(jobid))
-config = ConfigObj(wd + "/config.ini")
-svfSectors = config['toposcale']['svfSectors']
-svfMaxDist = config['toposcale']['svfMaxDist']
+
 
 homes = sorted(glob.glob(wd+"/sim/*"))
 tlib.compute_svf(homes[grid], svfSectors, svfMaxDist)
