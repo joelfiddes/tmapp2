e674227c3b3cc57c0f7b798d44d8243c78ccb140
Joel Fiddes <joelfiddes@gmail.com>
2020-11-12 09:47:54 +0100

    da code

--- rsrc/gridDA_FSM_hpc.R
+++ rsrc/gridDA_FSM_hpc.R
@@ -1,77 +1,39 @@
-source("./rsrc/PBS.R") 
-
+library(RcppCNPy)
 wd = "/home/caduff/sim/ch_tmapp_50"
-startSim <-   "1979-08-01" 
-endSim <-   "2019-09-30"
+
+HX <- as.matrix(read.table(paste0(wd, "/ensemble/HX")))
+obs <- read.table(paste0(wd, "/ensemble/obs"))
+obs=obs$V1
+
+#startSim <-   "1979-08-01" 
+#startSim <-   "1979-08-01" 
+#endSim <-   "2019-09-30"
 
 # fixed
-nens=8
-sdThresh=13
-R=0.016
-lp= read.csv(paste0(wd, "/listpoints.txt"))
+nens=49
+#sdThresh=13
+#R=0.016
+#lp= read.csv(paste0(wd, "/listpoints.txt"))
 fsca= read.csv(paste0(wd, "/meanSCA.csv"))
-obsTS = read.csv(paste0(wd,"/fsca_dates.csv"))
-fsca_dates= read.csv(paste0(wd, "/listpoints.txt"))
-ensembRes = read.csv(paste0(wd, "/ensembRes.csv"), header=F)
+fsca_dates = read.csv(paste0(wd,"/fsca_dates.csv"))
 fsca = fsca$meanvec
+weight = read.csv(paste0(wd, "/ensemble/weights.txt"), header=F)
 
-# make vector from df
-v=as.vector(t(ensembRes))
-# make 3d array days, samples, ensembles
-ensembRes2 <- array(v, dim = c(dim(ensembRes)[2],dim(ensembRes)[1]/nens, nens))
+weight = weight$V2
 
-# sequence of results
-totalTS <- seq(as.Date(startSim), as.Date(endSim), 1) # minus one because results from fsm are always one day shorter than defind start-endDAte
-da_dates <- totalTS[which(totalTS %in% as.Date(obsTS$x))]
-mod_da_dates_index <- which(totalTS %in% as.Date(obsTS$x))
-
-ensembRes2 <- ensembRes2[mod_da_dates_index, , ]
-
-# convert swe > sdThresh to snowcover = TRUE/1
-ensembRes2[ ensembRes2 <= sdThresh ] <- 0
-ensembRes2[ ensembRes2 > sdThresh ] <- 1
-
-
-# compute weighted  fsca by memebership
-#https://stackoverflow.com/questions/34520567/r-multiply-second-dimension-of-3d-array-by-a-vector-for-each-of-the-3rd-dimension
-Vect = lp$members
-
-# dimension that contains data first in making array, then reorder array using 'perm'
-varr <- aperm(array(Vect, dim = c(dim(ensembRes2)[2], dim(ensembRes2)[1], dim(ensembRes2)[3])), perm = c(2L, 1L, 3L))
-arr <- varr * ensembRes2
-
-# compute mean MOD fSCA per sample. HX is now T x Ens
-HX <- apply(arr, FUN = "sum", MARGIN = c(1,3)) / sum(lp$members)
-
-
-obs=fsca[1:250]/100
-sim=HX[1:250,]
-weight = PBS(sim, obs, R)
-#weight = PBS(HX[(DSTART:DEND)[obsind],], OBS[obsind], R)
-write.csv(as.vector(weight), paste0(wd,"/ensemble/weights.txt"), row.names=FALSE)
-write.csv(HX, paste0(wd,"/ensemble/HX.txt"), row.names=FALSE)
-
-write.csv(as.vector(weight), paste0(home,"/ensemble/weights.txt"), row.names=FALSE)
-
-
-OBS2PLOT <-obs
-#OBS2PLOT[naind]<-NA
-prior =sim
-
-weight = as.vector(weight)
-ndays = 1:250
+ndays = length(obs)
 lq=0.3
 uq=0.85
 
 
-png(paste0(home,"/ensemble/daplot.png"), width=800, height=400)
+png(paste0(wd,"/ensemble/daplot.png"), width=800, height=400)
 par(mfrow=c(1,2))
 
 # median
 med.post = c()
 for ( days in 1:ndays){
 print(days)
-mu = prior[ days, ]
+mu = HX[ days, ]
 w = weight
 wfill <- weight
 
@@ -85,7 +47,7 @@ med.post = c(med.post, med$y)
 low.post = c()
 for ( days in 1: ndays){
 
-mu = prior[ days, ]
+mu = HX[ days, ]
 w = weight
 wfill <- weight
 
@@ -99,7 +61,7 @@ low.post = c(low.post, med$y)
 high.post = c()
 for ( days in 1: ndays){
 
-mu = prior[ days, ]
+mu = HX[ days, ]
 w = weight
 wfill <- weight
 
@@ -111,13 +73,13 @@ high.post = c(high.post, med$y)
 
 
 
-# ======================= prior = ==========================================
+# ======================= HX = ==========================================
 
 # median
 med.pri = c()
 for ( days in 1: ndays){
 
-mu = prior[ days, ]
+mu = HX[ days, ]
 w = rep((1/nens),nens)
 wfill <- w
 
@@ -131,7 +93,7 @@ med.pri = c(med.pri, med$y)
 low.pri = c()
 for ( days in 1: ndays){
 
-mu = prior[ days, ]
+mu = HX[ days, ]
 w = rep((1/nens),nens)
 wfill <- w
 
@@ -145,7 +107,7 @@ low.pri = c(low.pri, med$y)
 high.pri = c()
 for ( days in 1: ndays){
 
-mu = prior[ days, ]
+mu = HX[ days, ]
 w = rep((1/nens),nens)
 wfill <- w
 
@@ -156,16 +118,11 @@ high.pri = c(high.pri, med$y)
 }
 
 
-date = seq(as.Date(startda), as.Date(endda),by='day')
-date=da_dates
-high.pri<-high.pri
-med.pri<-med.pri
-low.pri<-low.pri
-high.post<-high.post
-med.post<-med.post
-low.post<-low.post
+#date = seq(as.Date(startda), as.Date(endda),by='day')
+date=1:91
+
 #  xlim=c(date[182],date[355])
-plot(date,high.pri, col=NULL, type='l', main=paste('(A) fSCA'), ylab="fSCA (0-1)",xlab='', ylim=c(0,1))
+plot(date,high.pri, col=NULL, type='l', main=paste('(A) fSCA'), ylab="fSCA (0-1)",xlab='', ylim=c(0,0.2))
 #lines(low.pri, col='red')
 lines(date,med.pri ,col='red', lwd=3)
 #lines(high.post, col='blue')
@@ -186,15 +143,15 @@ lines(date,med.pri, col='red', lwd=3)
 #lines(high.post, col='blue')
 #lines(low.post, col='blue')
 lines(date,med.post, col='blue', lwd=3)
-points(date,OBS2PLOT, col='black', pch=3, lwd=4)
-lines(date, hx_determ_fsca[mod_da_dates_index ], col='green', lwd=3)
+points(date,obs, col='black', pch=3, lwd=4)
+#lines(date, hx_determ_fsca[mod_da_dates_index ], col='green', lwd=3)
 legend("topright", c("prior", "posterior", "obs", "open-loop") , col=c("red", "blue", "black", "green"), lty=c(1,1,NA),pch=c(NA,NA, 3))
 #abline(v=DSTART)
 #abline(v=DEND)
 #dev.off()
 
-abline(v=date[DSTART], lty=3)
-abline(v=date[DEND], lty=3)
+#abline(v=date[DSTART], lty=3)
+#abline(v=date[DEND], lty=3)
 #====================================================================
 #   # Now plot SWE (or HS)
 #====================================================================
@@ -204,29 +161,18 @@ abline(v=date[DEND], lty=3)
 #if(mode=='hs'){load(paste0(home, "//ensembRes_HS.rd")) ; ensembRes <- ensembResHS}
 #if (mode=='swe'){load(paste0(home, "//ensembRes.rd"))}
 
-ensembRes = read.csv(paste0(home, "//ensembRes.csv"), header=F)
 
-
-v=as.vector(t(ensembRes))
-# make 3d array days, samples, ensembles
-ensembRes2 <- array(v, dim = c(dim(ensembRes)[2],dim(ensembRes)[1]/nens, nens))
-
-# compute weighted  fsca by memebership
-#https://stackoverflow.com/questions/34520567/r-multiply-second-dimension-of-3d-array-by-a-vector-for-each-of-the-3rd-dimension
-Vect = lp$members
-varr <- aperm(array(Vect, dim = c(dim(ensembRes2)[2], dim(ensembRes2)[1], dim(ensembRes2)[3])), perm = c(2L, 1L, 3L))
-arr <- varr * ensembRes2
 
 
 # compute mean MOD swe per sample
-HX2 <- apply(arr, FUN = "sum", MARGIN = c(1,3)) / sum(lp$members)
+HX2 <- as.matrix(read.table(paste0(wd, "/ensemble/HX_swe")))
 
 
 #OBS2PLOT <-OBS
 #OBS2PLOT[naind]<-NA
-prior =HX2[1:250]
-weight = as.vector(weight)
-ndays = dim(HX2)[1]
+prior =HX2
+
+
 
 
 
@@ -326,12 +272,14 @@ high.pri = c(high.pri, med$y)
  # aod in km2 / #mm in km
 #pdf(paste0(wd,"/swe_grid.pdf"))
 
-if(mode=='swe'){cfact = aod/1000000}
-if (mode=="hs"){cfact=1}
+#if(mode=='swe'){cfact = aod/1000000}
+#if (mode=="hs"){cfact=1}
 cfact=1 # FIXED
 
 # ADD DATE
-date = seq(as.Date(obsTS$x[1]), as.Date(obsTS$x[250]),by='day')
+#date = seq(as.Date(obsTS$x[1]), as.Date(obsTS$x[250]),by='day')
+date=1:91
+
 #date=da_dates
 
 #,xlim=as.Date(c(date[100],date[355]))
--- tmapp_hpc_HX.py
+++ tmapp_hpc_HX.py
@@ -1,18 +1,86 @@
-# run python tmapp_hpc_perturb.py #WD
-import subprocess
-import sys
-import os
 from configobj import ConfigObj
 import glob
+import pandas as pd
+import numpy as np
+import subprocess
 
-wd= sys.argv[1] #'/home/joel/sim/qmap/ch_tmapp_10/' 
+wd= sys.argv[1] # wd ='/home/caduff/sim//ch_tmapp_50/' 
+da_year=sys.argV[2] # da_year=2003
 config = ConfigObj(wd + "/config.ini")
 
+#constants
+sdThresh=0
+R=0.016
+R=0.0001
 # ===============================================================================
-#	Make ensemble.csv (peturb pars)
+#	Kode
 # ===============================================================================
-
+fsca= pd.read_csv(wd + "/meanSCA.csv")
+fsca_dates= pd.read_csv(wd + "/fsca_dates.csv")
 ensemb_dir = wd+"/ensemble"
 N = config['ensemble']['members']
 
-# original construction of ensemRes object does not scale (sample*ensembles*time)
+lp = pd.read_csv(wd + "/listpoints.txt")
+
+# find all ensemble result files
+ensembleResults =glob.glob(wd + "/ensembRes*")
+
+# loop over to do processing
+data=[]
+for eres in ensembleResults:
+	ensembRes2 = pd.read_csv(eres, header=None)
+
+	ensembRes2[ ensembRes2 <= sdThresh ] = 0
+	ensembRes2[ ensembRes2 > sdThresh ] = 1
+	Vect = lp.members
+	arr = ensembRes2.transpose()*Vect
+	HXi = arr.sum(axis=1)/sum(lp.members)
+	data.append(HXi)
+
+HX = np.asarray(data).transpose()
+
+# make the real value matrix fro plotting
+data=[]
+for eres in ensembleResults:
+	ensembRes2 = pd.read_csv(eres, header=None)
+	ensembRes2[ensembRes2>1000]=1000
+	Vect = lp.members
+	arr = ensembRes2.transpose()*Vect
+	HXi = arr.sum(axis=1)/sum(lp.members)
+	data.append(HXi)
+
+HX_swe = np.asarray(data).transpose()
+
+
+# cut all values > 10000
+# extract obs for da year
+startIndex = fsca_dates[fsca_dates.iloc[:,0]==str(da_year)+"-03-31"].index.values     
+endIndex = fsca_dates[fsca_dates.iloc[:,0]==str(da_year)+"-06-30"].index.values 
+
+Y=fsca[int(startIndex):int(endIndex)]/100
+
+
+# particle batch smoothert
+
+No=len(Y)
+Rinv=np.array([(1/R)]*No)
+mat = np.ones((1,HX.shape[1])) 
+
+# Calculate the likelihood.
+Inn= np.kron(mat,Y) -HX
+
+# with %*% gives NA
+EObj=Rinv.dot(Inn**2)                  # [1 x Ne] ensemble objective function.
+
+LH=np.exp(-0.5*EObj)                     # Scaled likelihood. 
+
+# Calculate the posterior weights as the normalized likelihood. 
+w=LH/sum(LH)                        # Posterior weights.
+
+pd.Series(w).to_csv( wd + "/ensemble/weights.txt")
+np.savetxt(wd + "/ensemble/HX", HX)
+np.savetxt(wd + "/ensemble/HX_swe", HX_swe)
+np.savetxt(wd + "/ensemble/obs", Y)
+
+cmd = [ "Rscript",   "./rsrc/gridDA_FSM_hpc.R"]
+subprocess.check_output(cmd)
