5f29b41fd02774a533dbe395c442a9b6ae7b98e8
Joel Fiddes <joelfiddes@gmail.com>
2020-11-15 16:09:49 +0100

    python slurm controller

--- tmapp_hpc_run.sh
+++ tmapp_hpc_run.sh
@@ -1,10 +1,14 @@
-# Example bash /tmapp_hpc_run.sh /home/caduff/sim/ccamm_inter 100 1200 2003
+# Example bash tmapp_hpc_run.sh /home/caduff/sim/ccamm_inter 100 1200 2003
 
 # Args:
 #	$1: is working directory
 # 	$2: number of months in sim rounded up to nearest 100
 # 	$3: number of samples rounded up to nearest 100
 # 	$4: data assimilation year corresponding to melt period
+
+NGRIDS=6
+NENSEMBLE=100
+NJOBS=100
 
 if [[ $# -eq 0 ]] ; then
     echo 'Working directory needed as Arg1'
@@ -31,7 +35,7 @@ rm LOG*
 
 SBATCHID=$(sbatch slurm_setup.sh $1)
 jid1=${SBATCHID//[!0-9]/}
-echo $SBATCHID
+
 # = Parallel job by N era5 grids  ==============================================
 
 #Edit:
@@ -40,9 +44,10 @@ echo $SBATCHID
 # python2 script: tmapp_hpc_svf.sh 
 # computes, svf, surface toosub
 
-SBATCHID=$(sbatch  --dependency=afterany:$jid1 slurm_svf.sh $1)
+# array = number grids
+SBATCHID=$(sbatch  --dependency=afterany:$jid1 --array=1-$NGRIDS  slurm_svf.sh $1)
 jid2=${SBATCHID//[!0-9]/}
-echo $SBATCHID
+
 # = Parallel job by N jobs (normally 100) x months ==============================
 
 #Arg2 is a round number greater than number of months to compute in order to 
@@ -51,9 +56,10 @@ echo $SBATCHID
 # perhaps edit #<SBATCH --array=1-100 >
 # jobs per processor must be a whole number
 
-SBATCHID=$(sbatch  --dependency=afterany:$jid2 --array=1-50 slurm_tscale.sh $1 $2)
+# array = reasonable number
+SBATCHID=$(sbatch  --dependency=afterany:$jid2 --array=1-$NJOBS slurm_tscale.sh $1 $2)
 jid3=${SBATCHID//[!0-9]/}
-echo $SBATCHID
+
 # = Parallel job by N jobs (normally 100) x samples ============================
 
 #Arg2 is a round number greater than number of samples to compute in order to 
@@ -62,32 +68,27 @@ echo $SBATCHID
 # perhaps edit #<SBATCH --array=1-100 >
 # jobs per processor must be a whole number
 
-SBATCHID=$(sbatch  --dependency=afterany:$jid3  slurm_sim.sh $1 $3)
+# array = reasonable number
+SBATCHID=$(sbatch  --dependency=afterany:$jid3  --array=1-$NJOBS slurm_sim.sh $1 $3)
 jid4=${SBATCHID//[!0-9]/}
-echo $SBATCHID
 
 
-SBATCHID=$(sbatch  --dependency=afterany:$jid4  slurm_perturb.sh $1)
+
+# Generate meteo purturbations 
+SBATCHID=$(sbatch  --dependency=afterany:$jid4  --array=1 slurm_perturb.sh $1)
 jid5=${SBATCHID//[!0-9]/}
-echo $SBATCHID
 
-# number of ensembles declared in here as array number
-SBATCHID=$(sbatch  --dependency=afterany:$jid5  slurm_da.sh $1 $4)
+# run ensembles get results, array = number of ensembles (in config.ini)
+SBATCHID=$(sbatch  --dependency=afterany:$jid5  --array=1-$NENSEMBLE slurm_da.sh $1 $4)
 jid6=${SBATCHID//[!0-9]/}
-echo $SBATCHID
 
 # compute mean Modis fSCA
-SBATCHID=$(sbatch  --dependency=afterany:$jid6  slurm_modis.sh $1)
+SBATCHID=$(sbatch  --dependency=afterany:$jid6 --array=1 slurm_modis.sh $1)
 jid7=${SBATCHID//[!0-9]/}
-echo $SBATCHID
 
 # run PBS and plots
-# echo "run pbs"
-# sbatch --dependency=singleton --job-name=modis slurm_pbs.sh  $1  $4
-# echo "Done!"
-
-SBATCHID=$(sbatch  --dependency=afterany:$jid7  slurm_pbs.sh  $1  $4)
+SBATCHID=$(sbatch  --dependency=afterany:$jid7 --array=1 slurm_pbs.sh  $1  $4)
 jid8=${SBATCHID//[!0-9]/}
-echo $SBATCHID
+
 
 squeue -u caduff
