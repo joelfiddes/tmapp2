4b448c1d19e1037e3b0d5123e6541ec391bc695a
Joel Fiddes <joelfiddes@gmail.com>
2020-11-25 15:37:51 +0100

    cat

--- rsrc/spatialize_nc.R
+++ rsrc/spatialize_nc.R
@@ -39,16 +39,21 @@ rasters1 <- list.files(outroot, pattern=paste0(outname,"g"),full.names=TRUE, rec
 rast.list <- list()
 for(i in 1:length(rasters1)) { rast.list[i] <- raster(rasters1[i]) }
 
-	# And then use do.call on the list of raster objects
+# And then use do.call on the list of raster objects
 rast.list$fun <- mean
 rast.mosaic <- do.call(mosaic,rast.list)
 hist<-rast.mosaic
-	#plot(hist,zlim=zlim, col=mycol, main= "1980-2000 Hist ")
-writeRaster(hist, paste0(outroot,outname,"_map.nc"),overwrite=TRUE, format="CDF",     varname="HS", varunit="m",    longname="Snow height", xname="Longitude",   yname="Latitude", zname="Time (Month)")
+# plot(hist,zlim=zlim, col=mycol, main= "1980-2000 Hist ")
+# writeRaster(hist, paste0(outroot,outname,"_map.nc"),overwrite=TRUE, format="CDF",     varname="HS", varunit="m",    longname="Snow height", xname="Longitude",   yname="Latitude", zname="Time")
 
+hist_agg = aggregate(hist, fact=9,mean)
+writeRaster(hist_agg, paste0(outroot,outname,"_map.tif"),overwrite=TRUE)
 
 for (i in 1:length(rasters1)){
 	system(paste0("rm ",rasters1[i]))
 	}
 
 
+# call ncat
+# setwd(outroot)
+# system(paste0("cdo -b F64 -f nc2 mergetime *map.nc ", tsub_root,"/spatial/",column,"_COMPLETE.nc"))
