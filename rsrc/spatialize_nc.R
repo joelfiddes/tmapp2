a6dc8c70a66dccc0a1fd6ed623387e9735bcf3f2
Joel Fiddes <joelfiddes@gmail.com>
2020-11-25 12:43:04 +0100

    nc cat

--- rsrc/spatialize_nc.R
+++ rsrc/spatialize_nc.R
@@ -23,7 +23,7 @@ meanswe =read.csv(meanVar ,head=F )
 		landform=raster(paste0(tsub_root,"/sim/g",grid,"/landform.tif"))
 
 		rst <- subs(landform, df,by=1, which=2)
-		writeRaster(rst, paste0(tsub_root, "/g",grid,"_swe",outname,".tif"),overwrite=TRUE)
+		writeRaster(rst, paste0(tsub_root, outname, "g",grid,"_swe.tif"),overwrite=TRUE)
 		}
 
 
@@ -38,7 +38,7 @@ mycol=viridis(100)
 
 
 
-rasters1 <- list.files(tsub_root, pattern=paste0("*swe",outname,".tif"),full.names=TRUE, recursive=TRUE)
+rasters1 <- list.files(tsub_root, pattern=paste0(outname),full.names=TRUE, recursive=TRUE)
 rast.list <- list()
 for(i in 1:length(rasters1)) { rast.list[i] <- raster(rasters1[i]) }
 
@@ -47,7 +47,7 @@ rast.list$fun <- mean
 rast.mosaic <- do.call(mosaic,rast.list)
 hist<-rast.mosaic
 	#plot(hist,zlim=zlim, col=mycol, main= "1980-2000 Hist ")
-writeRaster(hist, paste0(tsub_root,"/",outname,"__map.tif"), overwrite=T)
+writeRaster(hist, paste0(tsub_root,outname,"__map.tif"), overwrite=T)
 
 
 for (i in length(rasters1)){
